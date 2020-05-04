7ae54e9d6999f0cd777b0d84e0171b1b71f2df54
Owen Dodd <owen.dodd@nytimes.com>
2020-05-03 21:53:27 -0400

    recolor text working baby

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,52 @@
-wrapChars;
-changeColor;
+$(document).ready(function () {
 
-$(function () {
-  var size = ["380px", "340px", "250px", "310px", "400px"];
+  wrapChars();
+  resize();
+  changeColor();
 
-  $("img").each(function () {
-    $(this).css({
-      height: size[Math.floor(Math.random() * size.length)],
+  
+  function resize() {
+    var size = ["380px", "340px", "250px", "310px", "400px"];
+
+    $("img").each(function () {
+      $(this).css({
+        height: size[Math.floor(Math.random() * size.length)],
+      });
     });
-  });
+  };
+
+  function wrapChars() {
+
+    var letter = $("p, a").contents();
+
+		letter.each(function(){
+				console.log(letter);
+				var $this = $(this);
+				$this.replaceWith($this.text().replace(/(\S)/g, "<letter>$&</letter>"));
+		});
+	};
+
+  function changeColor() {
+    var colors = [
+      "#177308",
+      "#177308",
+      "#4D6749",
+      "#498C20",
+      "#146D06",
+      "#57C614",
+    ];
+
+    $("letter").each(function () {
+      $(this).css({
+        transition: "color 3s ease-in-out",
+        color: colors[Math.floor(Math.random() * colors.length)],
+      });
+    });
+  };
+
+  
+
+
+  var myVar = setInterval(changeColor, 2000);
+
 });
-
-function changeColor() {
-  var colors = [
-    "#177308",
-    "#177308",
-    "#4D6749",
-    "#498C20",
-    "#146D06",
-    "#57C614",
-  ];
-
-  $("a").each(function () {
-    $(this).css({
-      transition: "color 5s ease-in-out",
-      color: colors[Math.floor(Math.random() * colors.length)],
-    });
-  });
-}
-
-function wrapChars() {
-  console.log(nodeType);
-
-  $("p, li")
-    .children()
-    .andSelf()
-    .not(".no-paint")
-    .contents()
-    .each(function () {
-      if (this.nodeType == 3) {
-        var $this = $(this);
-        $this.replaceWith($this.text().replace(/(\S)/g, "<letter>$&</letter>"));
-      }
-    });
-}
-
-var myVar = setInterval(changeColor, 5000);
